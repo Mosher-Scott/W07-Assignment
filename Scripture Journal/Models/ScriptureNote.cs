7aa7b69be70d9951dfec035543e979f818fdca74
Mosher-Scott <jorgemonkey81@byui.edu>
2020-02-18 13:41:18 -0700

    Worked on more

--- Scripture Journal/Models/ScriptureNote.cs
+++ Scripture Journal/Models/ScriptureNote.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Scripture_Journal.Models
 {
@@ -8,15 +9,22 @@ namespace Scripture_Journal.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
-        [DataType(DataType.Date)]
+        [Display(Name = "Entry Date"), DataType(DataType.Date)]
         public DateTime EntryDate { get; set; }
 
         // These hold the scripture book/chapter/verse info
+        [Display(Name ="Book")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(20)]
         public string ScriptureBook { get; set; }
+
+        [Display(Name = "Chapter")]
         public string ScriptureChapter { get; set; }
+
+        [Display(Name = "Verse")]
         public string ScriptureVerse { get; set; }
 
         // Hold the journal entry
+        [Display(Name = "Notes")]
         public string JournalEntry { get; set; }
 
     }
